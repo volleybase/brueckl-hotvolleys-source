694b489fcdfc50f4b442df99865239589caff850
volleybase <volleybase@users.noreply.github.com>
2019-08-16 19:04:23 +0200

    create a https test server

--- _server/bhv-server.js
+++ _server/bhv-server.js
@@ -1,7 +1,11 @@
 // var express = require('d:/workdir/brueckl-hotvolleys-source/node_modules/express')
 var express = require('express')
-var cors = require('cors')
+
+
+// -- BHV-Info - 5001 (currently not working) ---------------------------------
+
 var app = express()
+var cors = require('cors')
 
 // logging
 app.use(function(req, res, next) {
@@ -26,6 +30,8 @@ app.listen(5001, function () {
 })
 
 
+// -- volleybase --------------------------------------------------------------
+
 var volleybase = express()
 
 // logging
@@ -49,26 +55,65 @@ volleybase.listen(88, function () {
 })
 
 
-// bhv - new info pages
+// -- bhv - new info pages ----------------------------------------------------
+
 var bhv = express()
+var bhv2 = express()
 
 // logging
-bhv.use(function(req, res, next) {
+const logger = function(req, res, next) {
   console.log('%s %s %s', req.method, req.url, req.path);
   next();
-})
+}
+bhv.use(logger)
+bhv2.use(logger)
 
-// server
+// static files
 bhv.use(express.static('D:/workdir/BruecklHotvolleys.github.io'))
+bhv2.use(express.static('D:/workdir/BruecklHotvolleys.github.io'))
 
 // error info
-bhv.use(function (req, res, next) {
-  // res.status(404).send("BHV: Can't find that!")
+const onerror = function (req, res, next) {
   res.status(404).sendFile('D:/workdir/BruecklHotvolleys.github.io/404.html');
   console.log('BHV: %s %s %s %s', req.method, res.statusCode, req.url, req.path);
-})
+}
+bhv.use(onerror)
+bhv2.use(onerror)
 
 // start
 bhv.listen(5002, function () {
   console.log('BHV Info pages(5002)!')
 })
+
+// -- https server (BHV-Info-App) ---------------------------------------------
+
+// https-server (http 1.1) or spdy-server (http 2)
+var useHttp2 = true
+var https = useHttp2 ? require('spdy') : require('https')
+
+var fs = require('fs');
+var options = {
+  pfx: fs.readFileSync('D:/workdir/brueckl-hotvolleys-source/_server/certificate9.pfx'),
+  passphrase: 'bhv.bhv.bhv'
+};
+if (useHttp2) {
+  // ??? options.allowHTTP1 = true
+}
+
+if (useHttp2) {
+  https
+    .createServer(options, bhv2)
+    .listen(5003, (error) => {
+      if (error) {
+        console.error(error)
+        return process.exit(1)
+      } else {
+        console.log('BHV Info pages with http2(5003)!')
+      }
+    })
+
+} else {
+  var httpsServer = https.createServer(options, bhv2);
+  httpsServer.listen(5003, 'localhost');
+  console.log('BHV Info pages with https(5003)!')
+}
