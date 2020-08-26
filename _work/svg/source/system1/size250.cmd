8e21b94d03b33aad73244bc34b8598fcd0c99186
volleybase <volleybase@users.noreply.github.com>
2020-08-26 17:31:08 +0200

    systems - updates

--- _work/svg/source/system1/size250.cmd
+++ _work/svg/source/system1/size250.cmd
@@ -5,20 +5,38 @@ rem set the path
 SET mypath=%~dp0
 call "%mypath%initpath.cmd"
 
-set pngs=%mypath%\verteidigung\png\
-set target=%mypath%..\..\..\..\system1\verteidigung\
-for /R %pngs% %%V IN (*.png) do call "%mypath%do_sizing.cmd" %%V %target%
+rem for /F %%i IN('echo aa bb') do echo %%i
+rem for /f "delims=" %%i in ('dir /B /A:D "%src%"') do rd /S /Q "%src%%%i"
+for /f "delims=" %%i in ('dir /B /A:D') do call :doit %%i
 
-set pngs=%mypath%\angriff\png\
-set target=%mypath%..\..\..\..\system1\angriff\
-for /R %pngs% %%V IN (*.png) do call "%mypath%do_sizing.cmd" %%V %target%
+goto :done
 
-set pngs=%mypath%\annahme\png\
-set target=%mypath%..\..\..\..\system1\annahme\
-for /R %pngs% %%V IN (*.png) do call "%mypath%do_sizing.cmd" %%V %target%
+rem set pngs=%mypath%\verteidigung\
+rem set target=%mypath%..\..\..\..\system1\verteidigung\
+rem for /R %pngs% %%V IN (*.png) do call "%mypath%do_sizing.cmd" %%V %target%
+rem
+rem set pngs=%mypath%\angriff\
+rem set target=%mypath%..\..\..\..\system1\angriff\
+rem for /R %pngs% %%V IN (*.png) do call "%mypath%do_sizing.cmd" %%V %target%
+rem
+rem set pngs=%mypath%\annahme\
+rem set target=%mypath%..\..\..\..\system1\annahme\
+rem for /R %pngs% %%V IN (*.png) do call "%mypath%do_sizing.cmd" %%V %target%
 
 :done
 endlocal
 
-rem exit /B %ERRORLEVEL%
 pause
+exit /B %ERRORLEVEL%
+rem goto :done2
+
+
+:doit
+
+set pngs=%mypath%%1\
+set target=%mypath%..\..\..\..\system1\%1\
+rem echo %1 %pngs% %target%
+for /R %pngs% %%V IN (*.png) do call "%mypath%do_sizing.cmd" %%V %target%
+
+exit /B
+
