10e936a1abb808ad32c5561acf01469e63f540b3
volleybase <volleybase@users.noreply.github.com>
2021-03-30 00:26:15 +0200

    presence

--- bhv-service-worker.js
+++ bhv-service-worker.js
@@ -3,7 +3,7 @@
 // the names and the files of the cache
 const CACHE = {
   'main': 'bhv-infoapp-main-513b8c934b25cfb7781a116ff0ec163d',
-  'data': 'bhv-infoapp-data-7f4d884aea613202dcf90a54d5d35116',
+  'data': 'bhv-infoapp-data-050fbce928f45456c42107464ea13036',
   'system1': 'bhv-infoapp-system1-396e168e59deacc76eaa7b4b1fbf8334',
   'system4': 'bhv-infoapp-system4-cce310d598413509c955f7a3dad58314',
   'system6': 'bhv-infoapp-system6-e0b78a63025a9acaa66928075091f4c6',
