f31440d619fab82a5c265c8641c242f60e1514bb
volleybase <volleybase@users.noreply.github.com>
2021-04-03 23:21:32 +0200

    statistics u16

--- bhv-service-worker.js
+++ bhv-service-worker.js
@@ -2,7 +2,7 @@
 
 // the names and the files of the cache
 const CACHE = {
-  'main': 'bhv-infoapp-main-de1914629d6daa9c52f5cdd8607eff1b',
+  'main': 'bhv-infoapp-main-12dd2df316f17954909bdc844f285d0a',
   'data': 'bhv-infoapp-data-482f7641ca48c67449ae471d7fbde0dc',
   'system1': 'bhv-infoapp-system1-396e168e59deacc76eaa7b4b1fbf8334',
   'system4': 'bhv-infoapp-system4-cce310d598413509c955f7a3dad58314',
@@ -10,7 +10,7 @@ const CACHE = {
   'favicons': 'bhv-infoapp-favicons-330b73fc504d548333704fb32f4ddb85',
   'info': 'bhv-infoapp-info-14545db906177853eedc9ed447cbd3db',
   'statistics_19': 'bhv-infoapp-statistics_19-1721c54a73e426c061a4da259e61d823',
-  'statistics_21': 'bhv-infoapp-statistics_21-75d04719e4d00993022d7ab6aba72fc2',
+  'statistics_21': 'bhv-infoapp-statistics_21-05a113d3ccf1a56157267163db3316cb',
   'teambuilding_19': 'bhv-infoapp-teambuilding_19-9aeecfc5514d12fd7a4ac6ca488835d7',
   'teambuilding_21': 'bhv-infoapp-teambuilding_21-1d935a12406f3b78b70a9ac5097f9e0a',
   'videofeedback': 'bhv-infoapp-videofeedback-b2bf9c76a8cca61e78575a5946bd20c0'
@@ -371,7 +371,8 @@ const FILES = {
   'statistics_21': [
     '/statistics/21/bhv1_gd.html',
     '/statistics/21/bhv2_gd.html',
-    '/statistics/21/bhv3_gd.html'
+    '/statistics/21/bhv3_gd.html',
+    '/statistics/21/u16_4.html'
   ],
   'teambuilding_19': [
     '/teambuilding/br3_19/',
